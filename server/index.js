47762bf7c3696551c4521d647f64895bbf59a220
Coffee-Expert <abhishekevingomesofficial@gmail.com>
2024-08-05 19:34:22 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -16,7 +16,13 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use("/auth", authRoutes);
 
@@ -37,6 +43,10 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello World!!");
 });
 const PORT = 6001;
 const uri =
