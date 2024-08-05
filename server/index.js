690d8dfcffa2a94a52ca289fea4189d62075a5a6
Coffee-Expert <abhishekevingomesofficial@gmail.com>
2024-08-05 19:47:30 +0530

    updated index origin

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://flux-meetings.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
