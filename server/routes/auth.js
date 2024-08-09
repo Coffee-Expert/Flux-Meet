3bd2f24bf5db65ab0cd7687a1f689d6549b2d09d
Coffee-Expert <abhishekevingomesofficial@gmail.com>
2024-08-09 15:59:06 +0530

    patch

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { login, register } from "../controllers/auth.js";
 
-const router = express.Router();
+const authRoutes = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+authRoutes.post("/register", register);
+authRoutes.post("/login", login);
 
-export default router;
+export default authRoutes;
